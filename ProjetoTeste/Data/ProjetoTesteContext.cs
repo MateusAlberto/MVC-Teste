ed90ed62c3629168a97c8d48135f40147d202df6
Mateus Alberto Dorna <madof007@gmail.com>
2019-01-23 16:45:45 -0200

    Other entities and second migration

--- ProjetoTeste/Data/ProjetoTesteContext.cs
+++ ProjetoTeste/Data/ProjetoTesteContext.cs
@@ -13,6 +13,8 @@ namespace ProjetoTeste.Models
         {
         }
 
-        public DbSet<ProjetoTeste.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- ProjetoTeste/Models/Department.cs
+++ ProjetoTeste/Models/Department.cs
@@ -1,8 +1,33 @@
-﻿namespace ProjetoTeste.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoTeste.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
