298ab656b1f7c5016720a5c7d1d560e0de6d250d
Mateus Alberto Dorna <madof007@gmail.com>
2019-01-24 00:23:17 -0200

    Foreign key not null (referential integrity)

--- ProjetoTeste/Migrations/20190124021702_DepartmentForeignKey.Designer.cs
+++ ProjetoTeste/Migrations/20190124021702_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoTeste.Models;
 
 namespace ProjetoTeste.Migrations
 {
     [DbContext(typeof(ProjetoTesteContext))]
-    partial class ProjetoTesteContextModelSnapshot : ModelSnapshot
+    [Migration("20190124021702_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
