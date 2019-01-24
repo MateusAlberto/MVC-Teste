303fe437f6c2dd966bb65b49972b9da9050cd40f
Mateus Alberto Dorna <madof007@gmail.com>
2019-01-24 11:29:25 -0200

    Update seller and custom service exception

---
+++ ProjetoTeste/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjetoTeste.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
