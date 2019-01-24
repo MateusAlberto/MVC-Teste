5aa5e80945fed60b495199701b9015f5eb4ee1c6
Mateus Alberto Dorna <madof007@gmail.com>
2019-01-24 00:49:26 -0200

    SellerFormViewModel and Department select component

---
+++ ProjetoTeste/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoTeste.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProjetoTeste.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoTesteContext _context;
+
+        public DepartmentService(ProjetoTesteContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
